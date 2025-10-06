0950415512dc76a8a44374f2cb4e9faaf9f018c2
skevisvagelis769 <skevisvagelis769@gmail.com>
2025-10-06 21:44:14 +0300

    Created environment variable and added a frontend file server on app.go so that main.css gets accessed

--- src/app.go
+++ src/app.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 
 	"BLOG_API/handlers"
 )
@@ -10,7 +11,11 @@ import (
 // TODO: MAKE CI/CD DO NOT MAKE CHANGES WITHOUT THE WEBSITE RUNNING ON THE SERVER!!!
 // TODO: connect the blog docker container and the mysql container to a single network, the ip for the db go file uses is 172.35.0.3
 func main() {
+	frontendPath := os.Getenv("FRONTEND_PATH")
+	fs := http.FileServer(http.Dir(frontendPath))
+	http.Handle("/src/frontend", http.StripPrefix("/src/frontend", fs))
 	mux := http.NewServeMux()
+
 	mux.HandleFunc("/", serveFiles)
 	mux.HandleFunc("GET /blog", handlers.GetALlPost)
 	mux.HandleFunc("POST /blog", handlers.CreatePost)

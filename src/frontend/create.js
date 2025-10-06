98928b5a361b63bc5a5d93d7ceb5ebcff465aa4e
skevisvagelis769 <skevisvagelis769@gmail.com>
2025-10-06 22:34:00 +0300

    succesful post function

--- src/frontend/create.js
+++ src/frontend/create.js
@@ -5,5 +5,19 @@ tags=document.getElementById("tags")
 submit=document.getElementById("submit")
 
 submit.addEventListener('click',e =>{
+    console.log(title.value,content.value,category.value,tags.value)
     console.log("clicked")
+    fetch('http://localhost:8080/blog',{
+        method: "POST",
+        headers: {"Content-type":"application/json"},
+        body: JSON.stringify({
+            "title":`${title.value}`,
+            "content":`${content.value}`,
+            "category":`${category.value}`,
+            "tags":`${tags.value}`
+        })
+    }).then(res => res.json())
+    .then(response =>{
+        console.log(response)
+    })
 })
